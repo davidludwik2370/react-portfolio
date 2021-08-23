dfc85452d285af53b586ade1371022d44626993f
David Ludwik <davidludwik237@gmail.com>
2021-08-23 02:15:42 -0400

    functionality

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -2,6 +2,27 @@ import React from 'react';
 import './pageAssetts/css/style.css';
 
 export default function Blog() {
+  React.useEffect(() => {
+    document
+    .querySelector('.box1')
+    .addEventListener('click',()=>{window.location.href = "https://github.com/mcall0147/fit-commit";})
+
+    document
+    .querySelector('.box2')
+    .addEventListener('click',()=>{window.location.href = "https://github.com/davidludwik2370/project-1_RDB";})
+
+    document
+    .querySelector('.box3')
+    .addEventListener('click',()=>{window.location.href = "https://glacial-lake-27691.herokuapp.com/";})
+
+    document
+    .querySelector('.box4')
+    .addEventListener('click',()=>{window.location.href = "https://davidludwik2370.github.io/hw-5/";})
+
+    document
+    .querySelector('.box5')
+    .addEventListener('click',()=>{window.location.href = "https://github.com/davidludwik2370/hw-10";})
+  });
   return (
     <div>
       <h1>Blog Page</h1>
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Document, Page } from 'react-pdf';
+import Pdf from './pageAssetts/docs/David_Ludwik_Resume.pdf';
 
 export default function Home() {
   return (
     <div>
-      <span><a href = "./David_Ludwik_Resume.pdf">Resume</a></span>
+      <a href = {Pdf} target = "_blank">Download Pdf</a>
+      {/* <span><a href = "./pageAssetts/docs/David_Ludwik_Resume.pdf">Resume</a></span> */}
+      {/* <Document
+        file={Pdf}
+        // onLoadSuccess={onDocumentLoadSuccess}
+      >
+       <Page pageNumber={1} />
+      </Document> */}
     </div>
   );
 }
