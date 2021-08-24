0621cc05b85901f27cea2a527c03b3e2cb6401a8
David Ludwik <davidludwik237@gmail.com>
2021-08-24 11:53:06 -0400

    contact info

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,7 +4,16 @@ export default function Contact() {
   return (
     <div>
       <h1>Contact Page</h1>
-      <span><a href = "mailto: davidludwik237@gmail.com">Contact Me</a></span>
+      <div>
+        <a href = "mailto: davidludwik237@gmail.com">Contact Me</a>
+      </div>
+      <div>
+        <a href="https://github.com/davidludwik2370" target="_blank">Github</a>
+      </div><div>
+        <a href = "https://www.linkedin.com/in/david-ludwik">LinkedIn</a>
+      </div>
+      
+      
     </div>
   );
 }
